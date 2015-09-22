e2b49e483fc792ac424accf8ddb71b1b807bc109
三上航人 <mkm@shuto.info>
2015-09-22 17:55:26 +0900

    postの処理をうごくように修正

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 5010;
 var NCMB = require("ncmb");
 var ncmb = new NCMB("07f9eb21dd8db1dc715c77d272561004dd1941a3adc961f6d4e51d649d85922f", "ad947567899b91be5982e7102f0723b477087d401d2d4eb2e71d12f324be8011");
@@ -7,10 +8,11 @@ var ncmb = new NCMB("07f9eb21dd8db1dc715c77d272561004dd1941a3adc961f6d4e51d649d8
 var TwilioData = ncmb.DataStore("twilioData");
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/setVoice', function(req, res){
   var data = new TwilioData({
-    url: request.body.userRecordingUrl,
-    time: request.body.userRecordingDuration
+    url: req.param('userRecordingUrl'),
+    time: req.param('userRecordingDuration')
   });
   data.save().then(function(data){
     console.log(data);
