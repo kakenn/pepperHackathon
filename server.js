a197b620fad6d86d16aba8bee46524a2c14508f5
三上航人 <mkm@shuto.info>
2015-09-22 17:13:07 +0900

    いろいろ追加

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5010;
+var NCMB = NCMB || require("../lib/ncmb");
+var ncmb = new NCMB("07f9eb21dd8db1dc715c77d272561004dd1941a3adc961f6d4e51d649d85922f", "ad947567899b91be5982e7102f0723b477087d401d2d4eb2e71d12f324be8011");
+
+var TwilioData = ncmb.DataStore("twilioData");
 
 app.use(express.static('public'));
-
+app.get('/setVoice', function(req, res){
+  var data = new TwilioData({
+    /*
+    url: req.param('RecordingUrl'),
+    time: req.param('RecordingDuration')
+    */
+    url: 'test',
+    time: 'test'
+  });
+  data.save().then(function(data){
+    console.log(data);
+  }).catch(function(err){
+  console.log(err);
+});
+});
 app.listen(port);
 console.log("surver running at localhost:" + port);
+
